fdb01831bc2865e5ae7e5db21ad91bbf6d6f4caf
chl850405 <christimarchetti@gmail.com>
2021-12-29 23:59:20 -0500

    page-template

--- src/page-template.js
+++ src/page-template.js
@@ -1,7 +1,107 @@
-  const generatePage = (employeeData) => {
-    console.log(employeeData)
+const generateTeam = team => {
+
+  // create the manager html
+  const generateManager = manager => {
+    return `
+    <div class=" employee employee-card " style="width: 18rem;">
+    <div class="cardHeader text-white text-center bg-primary">
+      <h2 class="title h6">${manager.getName()}</h2>
+      <h3 class="title h6"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body border">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item id">ID:${manager.getId()}</li>
+        <li class="list-group-item email">Email:${manager.getEmail()}</li>
+        <li class="list-group-item office">Office number:${manager.getOfficeNumber()}</li>
+      </ul>
+    </div>
+    </div>
+    `;
+};
+
+// create the html for engineers
+const generateEngineer = engineer => {
   return `
-  <!doctype html>
+    <div class="employee employee-card " style="width: 18rem">
+    <div class="cardHeader text-white text-center bg-primary">
+      <h2 class="title h6">${engineer.getName()}</h2>
+      <h3 class="title h6"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body border">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item id">ID:${engineer.getId()}</li>
+        <li class="list-group-item email">Email:${engineer.getEmail()}</li>
+        <li class="list-group-item github">Github:<a href="https://github.com/${engineer.getGithub()}" target="_blank"</li>
+      </ul>
+    </div>
+    </div>
+        `;
+  };  
+
+     // create the html for interns
+    const generateIntern = intern => {
+      return `
+    <div class="employee employee-card " style="width: 18rem;">
+    <div class="cardHeader bg-primary text-white text-center">
+      <h2 class="title h6">${intern.getName()}</h2>
+      <h3 class="title h6"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body border">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item id">ID:${intern.getRole()}</li>
+        <li class="list-group-item email">Email:${intern.getEmail()}</li>
+        <li class="list-group-item school">School:${intern.getSchool()}</li>
+      </ul>
+    </div>
+    </div>
+`;
+};
+generateHTML = (data)
+=> {
+  // array for cards 
+  pageArray = []; 
+
+  for (let i = 0; i < data.length; i++) {
+      const employee = data[i];
+      const role = employee.getRole(); 
+
+
+      // call manager function
+      if (role === 'Manager') {
+          const managerCard = generateManager(employee);
+
+          pageArray.push(managerCard);
+      }
+
+      // call engineer function
+      if (role === 'Engineer') {
+          const engineerCard = generateEngineer(employee);
+
+          pageArray.push(engineerCard);
+      }
+
+      // call intern function 
+      if (role === 'Intern') {
+          const internCard = generateIntern(employee);
+
+          pageArray.push(internCard);
+      }
+      
+  }
+      // joining strings 
+      const employeeCards = pageArray.join('')
+
+      // return to generated page
+      const generateTeam = generateTeamPage(employeeCards); 
+      return generateTeam;
+  
+}
+}
+
+module.exports = team => {
+
+  return `
+  <!DOCTYPE html>
   <html lang="en">
       <head> 
       <!-- Required meta tags -->
@@ -12,7 +112,7 @@
       <!-- Bootstrap CSS -->
       <script src="https://kit.fontawesome.com/12ce71a7d2.js" crossorigin="anonymous"></script>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-      <link rel="stylesheet" href="./css/style.css">
+      <link rel="stylesheet" href="style.css">
 
       <title>My Team</title>
       </head>
@@ -24,21 +124,16 @@
         </div>
       </div>
       
-      <div class="container d-flex flex-wrap">
-
-      
-
+      <div class="container">
+        <div class="row>
+          <div d-flex teamArea col-12 justify-content-center" 
+              ${generateTeam(team)}
+              <div class="card-deck">
+              </div>
+          </div>
       </div>
-
-      <!-- Optional JavaScript -->
-      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-      </body>
-  </html> 
-  
+  </div>
+</body>
+</html>
   `;
-  };
-
-module.exports = generatePage;
+};
